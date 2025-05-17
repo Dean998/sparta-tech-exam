3b6636556446a7d05d8d8f4a7a0d3aed3890acd9
Dean <dean.b.98@hotmail.com>
2025-05-17 18:23:31 +0100

    feat: create trades service and controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TradesService } from './trades/trades.service';
+import { TradesModule } from './trades/trades.module';
 
 @Module({
-  imports: [],
+  imports: [TradesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TradesService],
 })
 export class AppModule {}
